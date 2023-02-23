a1a9d03dd55eb2e58bdf085c3960afa901ac9d4c
Amit Kumar Pal <myselfamit.ap23@gmail.com>
2023-02-23 23:56:36 +0530

    changed

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mongoose=require('mongoose');
 mongoose.set('strictQuery',false);
 
 //const mongoURI=process.env.MONGO_URL;
-//const mongoURI='mongodb+srv://dbinotebook:dbinotebook@cluster0.og6xdrl.mongodb.net/?retryWrites=true&w=majority'
-const mongoURI="mongodb://0.0.0.0:27017";
+const mongoURI="mongodb+srv://dbinotebook:dbinotebook@cluster0.og6xdrl.mongodb.net/?retryWrites=true&w=majority";
+//const mongoURI="mongodb://0.0.0.0:27017";
 
 
 const connectToMongo=()=>{
